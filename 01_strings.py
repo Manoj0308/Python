c41d1cbf3c5fb18efc4f2c80142927c6d76b8aad
manojyadav <yadavmanoj455@gmail.com>
2021-06-12 21:08:30 +0530

    Add three files which highlights string operations

---
+++ 01_strings.py
@@ -0,0 +1,7 @@
+# b = "Manoj's" # --> Use this if you have single quotes in your strings
+# b = 'Manoj"s'
+b = '''Manoj"s and 
+       Manoj's'''
+print(b)
+# print(type(b))
+
---
+++ 03_string_functions.py
@@ -0,0 +1,10 @@
+story = "once upon a time there was a youtuber named manoj who uploaded python course with notes manoj"
+
+# String Functions
+# print(len(story))
+# print(story.endswith("notes"))
+# print(story.count("c"))
+# print(story.capitalize())
+# print(story.find("upon"))
+print(story.replace("manoj", "CodeWithumesh"))
+ 
